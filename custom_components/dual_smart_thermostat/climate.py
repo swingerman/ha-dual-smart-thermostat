5343fbc3f09cd79f1240d586f2857c3d7f20ec4d
Miklos Szanyi <miklos.szanyi@gmil.com>
2020-09-16 10:32:26 +0300

    Update component to work with 0.114

--- custom_components/dual_smart_thermostat/climate.py
+++ custom_components/dual_smart_thermostat/climate.py
@@ -42,7 +42,6 @@ from homeassistant.helpers.event import (
     async_track_state_change_event,
     async_track_time_interval,
 )
-from homeassistant.helpers.reload import async_setup_reload_service
 from homeassistant.helpers.restore_state import RestoreEntity
 
 from . import DOMAIN, PLATFORMS
@@ -77,7 +76,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Required(CONF_SENSOR): cv.entity_id,
         vol.Optional(CONF_AC_MODE): cv.boolean,
         vol.Optional(CONF_MAX_TEMP): vol.Coerce(float),
-        vol.Optional(CONF_MIN_DUR): cv.positive_time_period,
+        vol.Optional(CONF_MIN_DUR): vol.All(cv.time_period, cv.positive_timedelta),
         vol.Optional(CONF_MIN_TEMP): vol.Coerce(float),
         vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
         vol.Optional(CONF_COLD_TOLERANCE, default=DEFAULT_TOLERANCE): vol.Coerce(float),
@@ -85,7 +84,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
         vol.Optional(CONF_TARGET_TEMP): vol.Coerce(float),
         vol.Optional(CONF_TARGET_TEMP_HIGH): vol.Coerce(float),
         vol.Optional(CONF_TARGET_TEMP_LOW): vol.Coerce(float),
-        vol.Optional(CONF_KEEP_ALIVE): cv.positive_time_period,
+        vol.Optional(CONF_KEEP_ALIVE): vol.All(cv.time_period, cv.positive_timedelta),
         vol.Optional(CONF_INITIAL_HVAC_MODE): vol.In(
             [HVAC_MODE_COOL, HVAC_MODE_HEAT, HVAC_MODE_OFF, HVAC_MODE_HEAT_COOL]
         ),
@@ -99,8 +98,6 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the generic thermostat platform."""
-
-    await async_setup_reload_service(hass, DOMAIN, PLATFORMS)
 
     name = config.get(CONF_NAME)
     heater_entity_id = config.get(CONF_HEATER)
@@ -212,24 +209,20 @@ class DualSmartThermostat(ClimateEntity, RestoreEntity):
         await super().async_added_to_hass()
 
         # Add listener
-        self.async_on_remove(
-            async_track_state_change_event(
-                self.hass, [self.sensor_entity_id], self._async_sensor_changed
-            )
+        async_track_state_change_event(
+            self.hass,
+            [self.sensor_entity_id, self.cooler_entity_id],
+            self._async_sensor_changed,
         )
-        self.async_on_remove(
-            async_track_state_change_event(
-                self.hass,
-                [self.heater_entity_id, self.cooler_entity_id],
-                self._async_switch_changed,
-            )
+        async_track_state_change_event(
+            self.hass,
+            [self.heater_entity_id, self.cooler_entity_id],
+            self._async_switch_changed,
         )
 
         if self._keep_alive:
-            self.async_on_remove(
-                async_track_time_interval(
-                    self.hass, self._async_control_heating, self._keep_alive
-                )
+            async_track_time_interval(
+                self.hass, self._async_control_heat_cool, self._keep_alive
             )
 
         @callback
